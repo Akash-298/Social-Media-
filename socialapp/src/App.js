d77f37a83f22420dadc196ca41cbd26bc010a2ad
Akash-298 <akash.c.c298@gmail.com>
2023-07-30 20:24:49 +0530

    learned styling in the mui and themes

--- socialapp/src/App.js
+++ socialapp/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button } from "@mui/material";
+import Sidebar from "./components/Sidebar";
+import Feed  from "./components/Feed.jsx";
+import Rightbar from "./components/Rightbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+     <Sidebar></Sidebar> 
+     <Feed></Feed>
+     <Rightbar></Rightbar> 
+  
+    </Box>
   );
 }
 
