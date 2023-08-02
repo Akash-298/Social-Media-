9d9928adcc34e9ded6e55e07d02c41eb9ac3393c
Akash-298 <akash.c.c298@gmail.com>
2023-08-02 15:04:30 +0530

    added the darkmode functionality to the switch

--- socialapp/src/App.js
+++ socialapp/src/App.js
@@ -1,22 +1,31 @@
-import { Box, Button, Stack } from "@mui/material";
+import { Box, Button, Stack, ThemeProvider, createTheme } from "@mui/material";
 import Sidebar from "./components/Sidebar";
 import Feed  from "./components/Feed.jsx";
 import Rightbar from "./components/Rightbar";
 import Navbar from "./components/Navbar";
+import { useState } from "react";
 
 function App() {
+  const [mode,setMode] =useState("light")
+  const darkTheme = createTheme({
+    palette:{
+      mode:mode
+    }
+  })
   return (
-    
-    <Box>
+    <ThemeProvider theme={darkTheme}>
+       <Box bgcolor={"background.default"} color={"text.primary"}>
       <Navbar></Navbar>
       <Stack direction="row" spacing={2} justifyContent="space-evenly">
-      <Sidebar></Sidebar> 
+      <Sidebar setMode={setMode} mode={mode}></Sidebar> 
      <Feed></Feed>
      <Rightbar></Rightbar> 
   
       </Stack>
     
     </Box>
+    </ThemeProvider>
+   
   );
 }
 
